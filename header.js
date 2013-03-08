fb938374bc132df819d7e5fff3e20f4defaef2d3
Doug Neiner <doug@pixelgraphics.us>
2013-03-07 21:19:26 -0600

    Added Anvil for a build system, switched to Uglify for minification, fixed a few minor things pointed out by JSHint

---
+++ header.js
@@ -0,0 +1,11 @@
+/*
+ * {{{name}}}
+ * {{{description}}}
+ * 
+ * Copyright (c) 2009 - 2013 {{{author}}}
+ * Dual licensed MIT or GPL
+ *   MIT (http://www.opensource.org/licenses/mit-license)
+ *   GPL (http://www.opensource.org/licenses/gpl-license)
+ *
+ * @version {{{version}}}
+ */
