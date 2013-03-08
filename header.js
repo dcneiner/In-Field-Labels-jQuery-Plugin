f5e7df7f5bfdf41b3f686929e5c4ab9392160886
Doug Neiner <doug@pixelgraphics.us>
2013-03-07 21:22:29 -0600

    Removed broken link, added link to github repository. Closes #29

--- header.js
+++ header.js
@@ -3,6 +3,7 @@
  * {{{description}}}
  * 
  * Copyright (c) 2009 - 2013 {{{author}}}
+ * Source: https://github.com/dcneiner/In-Field-Labels-jQuery-Plugin
  * Dual licensed MIT or GPL
  *   MIT (http://www.opensource.org/licenses/mit-license)
  *   GPL (http://www.opensource.org/licenses/gpl-license)
